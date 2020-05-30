becb286ac0a396f51c376364b1772edea297c97f
Mathïs Fédérico <mathfederico@gmail.com>
2020-05-30 17:18:31 +0200

    Updated color_scales in confusion matrix

--- classification_callback.py
+++ classification_callback.py
@@ -92,17 +92,22 @@ class WandbClassificationCallback(WandbCallback):
         np.fill_diagonal(confmatrix, 0)
 
         confmatrix = confmatrix.astype('float')
+        n_confused = np.sum(confmatrix)
         confmatrix[confmatrix == 0] = np.nan
-        confmatrix = go.Heatmap({'coloraxis': 'coloraxis1', 'x': self.labels, 'y': self.labels, 'z': confmatrix, 'hoverongaps':False, 'hovertemplate': 'y_true: %{x}<br>y_pred: %{y}<br>Count: %{z}<extra></extra>'})
+        confmatrix = go.Heatmap({'coloraxis': 'coloraxis1', 'x': self.labels, 'y': self.labels, 'z': confmatrix,
+                                 'hoverongaps':False, 'hovertemplate': 'Predicted %{y}<br>Instead of %{x}<br>On %{z} examples<extra></extra>'})
 
         confdiag = confdiag.astype('float')
+        n_right = np.sum(confdiag)
         confdiag[confdiag == 0] = np.nan
-        confdiag = go.Heatmap({'coloraxis': 'coloraxis2', 'x': self.labels, 'y': self.labels, 'z': confdiag, 'hoverongaps':False, 'hovertemplate': 'y_true: %{x}<br>y_pred: %{y}<br>Count: %{z}<extra></extra>'})
+        confdiag = go.Heatmap({'coloraxis': 'coloraxis2', 'x': self.labels, 'y': self.labels, 'z': confdiag,
+                               'hoverongaps':False, 'hovertemplate': 'Predicted %{y} just right<br>On %{z} examples<extra></extra>'})
 
         fig = go.Figure((confdiag, confmatrix))
         transparent = 'rgba(0, 0, 0, 0)'
-        fig.update_layout({'coloraxis1': {'colorscale': [[0, transparent], [0, 'rgba(180, 0, 0, 0.1)'], [1, 'rgba(180, 0, 0, 1)']], 'showscale': False}})
-        fig.update_layout({'coloraxis2': {'colorscale': [[0, transparent], [0, 'rgba(0, 180, 0, 0.1)'], [1, 'rgba(0, 180, 0, 1)']], 'showscale': False}})
+        n_total = n_right + n_confused
+        fig.update_layout({'coloraxis1': {'colorscale': [[0, transparent], [0, 'rgba(180, 0, 0, 0.05)'], [1, f'rgba(180, 0, 0, {max(0.2, (n_confused/n_total) ** 0.5)})']], 'showscale': False}})
+        fig.update_layout({'coloraxis2': {'colorscale': [[0, transparent], [0, f'rgba(0, 180, 0, {min(0.8, (n_right/n_total) ** 2)})'], [1, 'rgba(0, 180, 0, 1)']], 'showscale': False}})
 
         xaxis = {'title':{'text':'y_true'}, 'showticklabels':False}
         yaxis = {'title':{'text':'y_pred'}, 'showticklabels':False}
